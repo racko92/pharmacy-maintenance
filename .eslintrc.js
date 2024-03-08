d88d9be0d42011423088bc77ebcd08241d011c8e
racko92 <vladimir.rackovic.92@gmail.com>
2024-03-08 18:26:25 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    amd: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
+  rules: {},
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
